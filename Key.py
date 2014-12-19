c78d910877f4efa82370139e2c2a783e0f74f797
drakumus <rohancheeniyil@gmail.com>
2014-12-18 22:26:14 -0700

    Update Key.py

--- Key.py
+++ Key.py
@@ -1,31 +1,53 @@
 import pygame
 
+
 class RedKey(object):
+    def __init__(self, pos, keys):
+        keys.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
 
-	def __init__(self, pos, keys):
-		keys.append(self)
-		self.rect = pygame.Rect(pos[0],pos[1],16,16)
-	def collideKey(self,player):
-		if player.rect.colliderect(self.rect):
-			player.setRKey()
+    def collideKey(self, player):
+        if player.rect.colliderect(self.rect):
+            player.rKey = True
 
-class BlueKey(object)
-	def __init__(self, pos, keys):
-		keys.append(self)
-		self.rect = pygame.Rect(pos[0],pos[1],16,16)
-	def collideKey(self,player):
-		if player.rect.colliderect(self.rect):
-			player.setBKey
+class BlueKey(object):
+    def __init__(self, pos, keys):
+        keys.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+
+    def collideKey(self, player):
+        if player.rect.colliderect(self.rect):
+            player.bKey = True
 
 class GreenKey(object):
-	def __init__(self, pos, keys):
-		keys.append(self)
-		self.rect = pygame.Rect(pos[0],pos[1],16,16)
-	def collideKey(self,player):
-		if player.rect.colliderect(self.rect):
-			player.setGKey
+    def __init__(self, pos, keys):
+        keys.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
 
-class redDoor(object)
-	def __init__(self, pos, doors):
-		doors.append(self)
-		self.rect = pygame.Rect(pos[0], pos[1], 16,16)
+    def collideKey(self, player):
+        if player.rect.colliderect(self.rect):
+            player.gKey = True
+
+class RedDoor(object):
+    def __init__(self, pos, doors):
+        doors.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+    def collideDoor(self, player):
+        if player.rect.colliderect(self.rect):
+            player.rKey = False
+
+class BlueDoor(object):
+    def __init__(self, pos, doors):
+        doors.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+    def collideDoor(self, player):
+        if player.rect.colliderect(self.rect):
+            player.bKey = False
+
+class GreenDoor(object):
+    def __init__(self, pos, doors):
+        doors.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+    def collideDoor(self, player):
+        if player.rect.colliderect(self.rect):
+            player.gKey = False
